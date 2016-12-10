786e3f3f57e7b980666ac8017bfbf65ee286b739
zr <zr>
2016-12-11 01:25:40 +0800

    新闻删除完成

--- src/main/java/com/lebao/controller/hshb_index/IndexController.java
+++ src/main/java/com/lebao/controller/hshb_index/IndexController.java
@@ -6,6 +6,7 @@ import com.lebao.vo.NewsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,50 +19,77 @@ import java.util.List;
 public class IndexController {
     @Autowired
     NewsService newsService;
+
     /**
      * 首页
+     *
      * @return
      */
     @RequestMapping("/index")
     public ModelAndView index() {
         ModelAndView view = new ModelAndView();
-        try {  int curPage = 0;
+        try {
+            int curPage = 0;
             int pageSize = 5;
-            SearchBean searchBean =new SearchBean();
-            searchBean.getParamMap().put("typeId",1);
+            /**
+             * 新闻中心
+             */
+            SearchBean searchBean = new SearchBean();
+            searchBean.getParamMap().put("typeId", 1);
             searchBean.setCurrentPage(curPage);
             searchBean.setPageSize(pageSize);
-            List<NewsVo> indexNewsList=newsService.indexList(searchBean);
+            List<NewsVo> indexNewsList = newsService.indexList(searchBean);
             view.addObject("indexNewsList", indexNewsList);
-
-            SearchBean searchBean1 =new SearchBean();
-            searchBean1.getParamMap().put("typeId",2);
+            /**
+             * 信息公示
+             */
+            SearchBean searchBean1 = new SearchBean();
+            searchBean1.getParamMap().put("typeId", 2);
             searchBean1.setCurrentPage(curPage);
             searchBean1.setPageSize(pageSize);
-            List<NewsVo> indexInfosList=newsService.indexList(searchBean1);
+            List<NewsVo> indexInfosList = newsService.indexList(searchBean1);
             view.addObject("indexInfosList", indexInfosList);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         view.setViewName("index/index");
 
         return view;
     }
+
     @RequestMapping("/content")
     public ModelAndView content() {
         ModelAndView view = new ModelAndView();
         view.setViewName("index/content");
         return view;
     }
+
     @RequestMapping("/joboffer")
     public ModelAndView joboffer() {
         ModelAndView view = new ModelAndView();
         view.setViewName("index/joboffer");
         return view;
     }
+
+    /**
+     * 更多
+     * @param typeId
+     * @return
+     */
     @RequestMapping("/newslist")
-    public ModelAndView newslist() {
+    public ModelAndView newslist(	@RequestParam(value = "typeId", required = true) Long typeId ,
+                                     @RequestParam(value = "typeId", required = false,defaultValue = "1") Integer curPage) {
         ModelAndView view = new ModelAndView();
+        try {
+            SearchBean searchBean = new SearchBean();
+            searchBean.getParamMap().put("typeId", typeId);
+            searchBean.setCurrentPage(curPage);
+            searchBean.setPageSize(15);
+            List<NewsVo> indexNewsList = newsService.indexList(searchBean);
+            view.addObject("indexNewsList", indexNewsList);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         view.setViewName("index/newslist");
         return view;
     }
